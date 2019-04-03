058f8f1564a020fdc69c09f4764eb68ed7dfc076
William Kwok <wkwok1998@gmail.com>
2019-04-03 13:39:50 -0700

    Updated assignment 1 base code, WIP

---
+++ program_1/script1.py
@@ -0,0 +1,13 @@
+import sys
+
+INVALID_NUM_ARGS_ERROR = "At least two arguments not provided"
+INVALID_NUM_ERROR = "Invalid number provided"
+
+def multiply():
+    raise NotImplementedError("Not yet implemented")
+
+def raise_string():
+    raise NotImplementedError("Not yet implemented")
+
+def main(args):
+    raise NotImplementedError("Not yet implemented")
