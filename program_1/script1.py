be722febc83318538056dae1cf532518f99e4d98
William Kwok <wkwok1998@gmail.com>
2019-04-03 20:46:28 -0700

    Added some comments and NotImplementedErrors

--- program_1/script1.py
+++ program_1/script1.py
@@ -6,6 +6,7 @@ INVALID_NUM_ERROR = "Invalid number provided"
 def your_main_program(args):
     # Call your methods from here, using the
     # relevant items in args
+    raise NotImplementedError("Not yet implemented")
 
 def multiply():
     raise NotImplementedError("Not yet implemented")
@@ -13,5 +14,8 @@ def multiply():
 def raise_string():
     raise NotImplementedError("Not yet implemented")
 
+###################################
+## Do not modify this part below ##
+###################################
 if __name__ == '__main__':
     your_main_program(sys.argv)
--- program_3/script3.py
+++ program_3/script3.py
@@ -6,6 +6,10 @@ INVALID_NUM_ERROR = "Invalid number provided"
 def your_main_program(args):
     # Call your methods from here, using the
     # relevant items in args
+    raise NotImplementedError("Not yet implemented")
 
+###################################
+## Do not modify this part below ##
+###################################
 if __name__ == '__main__':
     your_main_program(sys.argv)
